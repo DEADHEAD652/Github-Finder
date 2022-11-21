af0c9dc9ade189e382fdd3e610fc4b7ca6be708b
DEADHEAD652 <hamzajjmalik@gmail.com>
2022-11-21 16:58:44 +0500

    Home about and not found pages

--- github-finder/src/App.js
+++ github-finder/src/App.js
@@ -1,13 +1,23 @@
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Navbar from "./components/layout/Navbar";
 import Footer from "./components/layout/Footer";
+import About from "./Pages/About";
+import Home from "./Pages/Home";
+import NotFound from "./Pages/NotFound";
 
 function App() {
   return (
     <Router>
       <div className="flex flex-col justify-between h-screen">
         <Navbar />
-        <main className="container mx-auto px-3  pb-12">cONTENT</main>
+        <main className="container mx-auto px-3  pb-12">
+          <Routes>
+            <Route path="/" element={<Home />}></Route>
+            <Route path="/about" element={<About />}></Route>
+            <Route path="/notfound" element={<NotFound />}></Route>
+            <Route path="/*" element={<NotFound />}></Route>
+          </Routes>
+        </main>
         <Footer />
       </div>
     </Router>
